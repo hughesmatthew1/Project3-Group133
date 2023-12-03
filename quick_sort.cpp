602609c1276abec2f254f09fd2983761d291f2dd
Morgan Evans <113069573+Morganevans3@users.noreply.github.com>
2023-12-03 18:17:06 -0500

    Add files via upload

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -11,47 +11,86 @@
 
 using namespace std;
 
-int partition(vector<int>& vec, int low, int high) {
+int partition(vector<StockData>& vec, int low, int high, int comp) {
     // change to struct instead of int
     // test
+    int i;
+    if (comp == 0) {
+        // open
 
-    // pick pivot
-    int pivot = vec[high];
-    // index of smaller element
-    int i = low - 1;
+        // pick pivot
+        float pivot = vec[high].open;
+        // index of smaller element
+        i = low - 1;
 
-    for (int j = low; j < high; j++) {
-        // if element is smaller than pivot
-        if (vec[j] < pivot) {
-            // increment index of smaller element
-            i++;
-            swap(vec[i], vec[j]);
+        for (int j = low; j < high; j++) {
+            // if element is smaller than pivot
+            if (vec[j].open < pivot) {
+                // increment index of smaller element
+                i++;
+                swap(vec[i], vec[j]);
+            }
         }
     }
+    else if (comp == 1) {
+        // close
+
+        // pick pivot
+        float pivot = vec[high].close;
+        // index of smaller element
+        i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            // if element is smaller than pivot
+            if (vec[j].close < pivot) {
+                // increment index of smaller element
+                i++;
+                swap(vec[i], vec[j]);
+            }
+        }
+    }
+    else if (comp == 2) {
+        // percent increase
+
+        // pick pivot
+        int pivot = vec[high].pIncrease;
+        // index of smaller element
+        i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            // if element is smaller than pivot
+            if (vec[j].pIncrease < pivot) {
+                // increment index of smaller element
+                i++;
+                swap(vec[i], vec[j]);
+            }
+        }
+    }
+
     swap(vec[i+1], vec[high]);
     return i + 1;
 }
 
-void quickSort(vector<int>& vec, int low, int high) {
+void quickSort(vector<StockData>& vec, int low, int high, int comp) {
     // change to struct instead of int
     // test
 
     if (low < high) {
-        int index = partition(vec, low, high);
+        int index = partition(vec, low, high, comp);
 
-        quickSort(vec, low, index-1);
-        quickSort(vec, index + 1, high);
+        quickSort(vec, low, index-1, comp);
+        quickSort(vec, index + 1, high, comp);
     }
 }
 
-vector<int> vecQuickSort(vector<int>& vecStock) {
+vector<StockData> vecQuickSort(vector<StockData>& vecStock, int comp) {
     // change to struct instead of int
     // test
 
     // create new vector to store the sorted vecStock
-    vector<int> sortedVec = vecStock;
+    vector<StockData> sortedVec = vecStock;
     int size = sortedVec.size();
-    quickSort(sortedVec, 0, size - 1);
+    quickSort(sortedVec, 0, size - 1, comp);
     return sortedVec;
 }
 
