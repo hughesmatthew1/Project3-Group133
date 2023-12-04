8ea4999084328ffb9ef3b904b82e5e5224624154
hughesmatthew1 <116681449+hughesmatthew1@users.noreply.github.com>
2023-12-03 19:34:42 -0500

    Update and rename quick_sort.cpp to quick_sort.h
    Included pragma statement, made a .h file

--- quick_sort.h
+++ quick_sort.h
@@ -2,6 +2,8 @@
 // Created by Morgan Evans on 12/3/23.
 //
 
+#pragma once
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -92,55 +94,4 @@ vector<StockData> vecQuickSort(vector<StockData>& vecStock, int comp) {
     int size = sortedVec.size();
     quickSort(sortedVec, 0, size - 1, comp);
     return sortedVec;
-}
-
-
-void readCSV(const char *filename) {
-    // change declarations
-    // test
-
-    ifstream file(filename, ios::in);
-    if (!file.is_open()) {
-        cout << "Error opening file: " << filename << std::endl;
-        return;
-    }
-
-    string line;
-    // Skip header
-    getline(file, line);
-
-    while (getline(file, line)) {
-        stringstream ss(line);
-        string date;
-        float openPrice, high, low, closePrice, adjClose;
-        float volume; // Assuming Volume is a long long type
-
-        // Assuming the CSV format is: Date,Open,High,Low,Close,Adj Close,Volume
-        getline(ss, date, ',');
-        ss >> openPrice;
-        ss.ignore(); // Ignore the comma
-        ss >> high;
-        ss.ignore(); // Ignore the comma
-        ss >> low;
-        ss.ignore(); // Ignore the comma
-        ss >> closePrice;
-        ss.ignore(); // Ignore the comma
-        ss >> adjClose;
-        ss.ignore(); // Ignore the comma
-        ss >> volume;
-
-        // this needs to be the struct you developed
-        stockData stock;
-        stock.symbol = "TTD";
-        stock.openPrice = openPrice;
-        stock.closePrice = closePrice;
-        stock.date = date;
-
-        vecStock.push_back(stock);
-    }
-    cout << vecStock[0].symbol << vecStock[0].date << vecStock[0].openPrice << vecStock[0].closePrice;
-    cout << vecStock[1].symbol << vecStock[1].date << vecStock[1].openPrice << vecStock[1].closePrice;
-    cout << vecStock[2].symbol << vecStock[2].date << vecStock[2].openPrice << vecStock[2].closePrice;
-
-    file.close();
 }
